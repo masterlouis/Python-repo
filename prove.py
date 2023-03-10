a4005833bae0fcd635e707ee8b3e6a6333222132
Luigi Mastromauro <gigi.mastro@hotmail.it>
2023-03-10 11:28:52 +0100

    cancellato prove.py

--- prove.py
+++
@@ -1,9 +0,0 @@
-def fai_la_pasta(tipo_pasta = "fusilli", sugo = True):
-    print("metti l'acqua")
-    print("fai bollire")
-    print("metti " + tipo_pasta)
-    if sugo:
-        print("con il sugo")
-    
-
-fai_la_pasta()
