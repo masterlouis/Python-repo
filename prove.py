b0ecbe7eb1105031e914ce4572ce20d68dcfc1a4
Luigi Mastromauro <gigi.mastro@hotmail.it>
2023-03-10 11:28:00 +0100

    prove.py

---
+++ prove.py
@@ -0,0 +1,9 @@
+def fai_la_pasta(tipo_pasta = "fusilli", sugo = True):
+    print("metti l'acqua")
+    print("fai bollire")
+    print("metti " + tipo_pasta)
+    if sugo:
+        print("con il sugo")
+    
+
+fai_la_pasta()
